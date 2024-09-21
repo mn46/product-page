7fbf723e70df9e029edfcf3eaa9c84835320f005
marta <m.nowak033@gmail.com>
2024-09-21 14:14:33 +0200

    Add custom colors and remove unnecessary styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{html,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryOrange: "hsl(26, 100%, 55%)",
+        primaryPaleOrange: "hsl(25, 100%, 94%)",
+        darkBlue: "hsl(220, 13%, 13%)",
+        darkGrayishBlue: "hsl(219, 9%, 45%)",
+        grayishBlue: "hsl(220, 14%, 75%)",
+        lightGrayishBlue: "hsl(223, 64%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
