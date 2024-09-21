e101361629dae38300c8f63f8f0ea18f48c86427
marta <m.nowak033@gmail.com>
2024-09-21 14:20:13 +0200

    Add layout to the navigation bar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import Navigation from "./Navigation";
 
 const NavBar = () => {
   return (
-    <nav>
-      <p className="font-black text-4xl">sneakers</p>
-      <Navigation />
+    <nav className="flex flex-row justify-between items-center">
+      <div className="flex flex-row gap-10 items-center">
+        <p className="font-black text-4xl">sneakers</p>
+        <Navigation />
+      </div>
       {/* shopping cart */}
       {/* profile */}
     </nav>
