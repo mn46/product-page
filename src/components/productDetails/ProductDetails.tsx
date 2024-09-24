529c77fc0f766fefb19080f248942e2642cc4be6
marta <m.nowak033@gmail.com>
2024-09-24 09:47:13 +0200

    Style details section to create hierarchy and align page with design

--- src/components/productDetails/ProductDetails.tsx
+++ src/components/productDetails/ProductDetails.tsx
@@ -24,40 +24,58 @@ const ProductDetails: React.FC = () => {
   return (
     <div>
       {/* pictures */}
-      <section>
-        <h4>{typedData?.brand}</h4>
-        <h2>{typedData?.name}</h2>
-        <p>{typedData?.description}</p>
+      <div className="flex flex-col gap-5">
+        <section>
+          <h4 className="uppercase text-primaryOrange text-sm font-semibold">
+            {typedData?.brand}
+          </h4>
+          <h2 className="text-4xl font-bold text-darkBlue">
+            {typedData?.name}
+          </h2>
+          <p className="text-darkGrayishBlue">{typedData?.description}</p>
 
-        <div>
-          <p>${typedData?.price}</p>
+          <div className="flex flex-row gap-3 items-center">
+            <p className="text-2xl font-bold text-darkBlue">
+              ${typedData?.price}
+            </p>
+            {typedData?.discount && (
+              <div className="bg-primaryPaleOrange px-2 py-0.5 rounded-lg text-primaryOrange text-sm font-semibold">
+                <p>{typedData?.discount}%</p>
+              </div>
+            )}
+          </div>
+
           {typedData?.discount && (
-            <div>
-              <p>{typedData?.discount}%</p>
-            </div>
+            <p className="text-darkGrayishBlue text-sm font-semibold line-through">
+              ${typedData?.price * (100 / typedData?.discount)}
+            </p>
           )}
+        </section>
+
+        {/* buttons */}
+        <div className="flex flex-row bg-lightGrayishBlue rounded-lg items-center">
+          <Button
+            variant="secondary"
+            onClick={() => handleDecrement()}
+            className="font-bold text-2xl"
+          >
+            -
+          </Button>
+          {amount}
+          <Button
+            variant="secondary"
+            onClick={() => handleIncrement()}
+            className="font-bold text-2xl"
+          >
+            +
+          </Button>
         </div>
 
-        {typedData?.discount && (
-          <p>${typedData?.price * (100 / typedData?.discount)}</p>
-        )}
-      </section>
-
-      {/* buttons */}
-      <div className="flex flex-row bg-lightGrayishBlue rounded-lg items-center">
-        <Button variant="secondary" onClick={() => handleDecrement()}>
-          -
-        </Button>
-        {amount}
-        <Button variant="secondary" onClick={() => handleIncrement()}>
-          +
+        <Button variant="primary" className="flex flex-row gap-2 items-center">
+          <CartIcon className="w-5 h-auto fill-white" />
+          Add to cart
         </Button>
       </div>
-
-      <Button variant="primary" className="flex flex-row gap-2 items-center">
-        <CartIcon className="w-5 h-auto fill-white" />
-        Add to cart
-      </Button>
     </div>
   );
 };
