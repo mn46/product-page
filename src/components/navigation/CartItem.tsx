e245224b5bb99ef7999e30257e1e48bc88f5151d
marta <m.nowak033@gmail.com>
2024-10-06 11:00:39 +0200

    Remove unnecessary prop and use value from the context

--- src/components/navigation/CartItem.tsx
+++ src/components/navigation/CartItem.tsx
@@ -1,38 +1,40 @@
-import React from "react";
-import { Cart } from "../../types/types";
+import React, { useContext } from "react";
 import DeleteIcon from "/icons/icon-delete.svg";
+import { CartContext } from "../../App";
 
 interface Props {
-  cartData: Cart;
   onClose: () => void;
 }
 
-const CartItem: React.FC<Props> = ({ cartData, onClose }) => {
+const CartItem: React.FC<Props> = ({ onClose }) => {
+  const { cart, setCart } = useContext(CartContext);
+
   const handleDelete = () => {
     localStorage.removeItem("cart");
+    setCart(null);
     onClose();
   };
 
   return (
-    cartData.product && (
+    cart.product && (
       <li className="p-4 flex flex-row gap-2">
         <img
-          src={cartData.product.thumbnails[0]}
-          alt={cartData.product.name}
+          src={cart.product.thumbnails[0]}
+          alt={cart.product.name}
           className="rounded-lg max-w-16 h-auto"
         />
         <div>
-          <h4>{cartData.product.name}</h4>
+          <h4>{cart.product.name}</h4>
           <div className="flex flex-row justify-between">
-            {cartData.amount ? (
+            {cart.amount ? (
               <p>
-                ${cartData.product.price} x {cartData.amount}{" "}
+                ${cart.product.price} x {cart.amount}{" "}
                 <span className="font-bold">
-                  ${cartData.product.price * cartData.amount}
+                  ${cart.product.price * cart.amount}
                 </span>{" "}
               </p>
             ) : (
-              <p>${cartData.product.price}</p>
+              <p>${cart.product.price}</p>
             )}
           </div>
         </div>
